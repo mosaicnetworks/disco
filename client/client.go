b1fd89f75c41395da23503e111862651d6089a85
arrivets <arrivets@gmail.com>
2020-03-18 15:35:07 +0000

    Add TLS on client side

--- client/client.go
+++ client/client.go
@@ -2,25 +2,79 @@ package client
 
 import (
 	"bytes"
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/json"
+	"encoding/pem"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 
 	"github.com/mosaicnetworks/disco/group"
+	"github.com/sirupsen/logrus"
 )
 
 // DiscoClient is a client for the Discovery API
 type DiscoClient struct {
-	url string
+	url      string
+	certFile string
+	client   *http.Client
+	logger   *logrus.Entry
 }
 
 // NewDiscoClient creates a new DiscoClient for a server hosted at the provided
 // url
-func NewDiscoClient(url string) *DiscoClient {
-	return &DiscoClient{
-		url: fmt.Sprintf("http://%s", url),
+func NewDiscoClient(url string, certFile string, logger *logrus.Entry) (*DiscoClient, error) {
+	tlscfg := &tls.Config{}
+
+	// If told to trust a certificate.
+	if certFile != "" {
+		// Load PEM-encoded certificate to trust.
+		certPEM, err := ioutil.ReadFile(certFile)
+		if err != nil {
+			return nil, err
+		}
+
+		// Create CertPool containing the certificate to trust.
+		roots := x509.NewCertPool()
+		if !roots.AppendCertsFromPEM(certPEM) {
+			return nil, errors.New("failed to import certificate to trust")
+		}
+
+		// Trust the certificate by putting it into the pool of root CAs.
+		tlscfg.RootCAs = roots
+
+		// Decode and parse the server cert to extract the subject info.
+		block, _ := pem.Decode(certPEM)
+		if block == nil {
+			return nil, errors.New("failed to decode certificate to trust")
+		}
+
+		cert, err := x509.ParseCertificate(block.Bytes)
+		if err != nil {
+			return nil, err
+		}
+
+		logger.Debugf("Trusting certificate %s with CN: %s", certFile, cert.Subject.CommonName)
+
+		// Set ServerName in TLS config to CN from trusted cert so that
+		// certificate will validate if CN does not match DNS name.
+		tlscfg.ServerName = cert.Subject.CommonName
 	}
+
+	res := &DiscoClient{
+		url:      fmt.Sprintf("https://%s", url),
+		certFile: certFile,
+		client: &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: tlscfg,
+			},
+		},
+		logger: logger,
+	}
+
+	return res, nil
 }
 
 // GetAllGroups returs all groups in a map where the key is the ID of the group
