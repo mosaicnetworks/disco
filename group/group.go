0d3b001b097554f648e0044fc603db306d9af02e
Jon Knight <jon@babble.io>
2020-04-14 17:32:19 +0100

    Last Block Index and group aging and pruning

--- group/group.go
+++ group/group.go
@@ -4,13 +4,14 @@ import "github.com/mosaicnetworks/babble/src/peers"
 
 // Group represents a Babble group of peers
 type Group struct {
-	ID           string
-	Name         string
-	AppID        string
-	PubKey       string
-	LastUpdated  int64
-	Peers        []*peers.Peer
-	GenesisPeers []*peers.Peer
+	ID             string
+	Name           string
+	AppID          string
+	PubKey         string
+	LastUpdated    int64
+	LastBlockIndex int64
+	Peers          []*peers.Peer
+	GenesisPeers   []*peers.Peer
 }
 
 // NewGroup generates a new Group
